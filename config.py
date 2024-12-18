f6ff5d0c23fc6920f5b5d781283a14b9b15fb0f3
AJB <adam.bro@hotmail.co.uk>
2024-12-18 19:52:28 +0100

    Second commit

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 DATABASE = os.path.join(BASE_DIR, 'database.db')
 SQLALCHEMY_DATABASE_URI = 'sqlite:///espresso.db'
-SECRET_KEY = 'your-secure-secret-key'
+SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-temporary-key'
 
 # Grinder Settings Validation
 GRINDER_SETTINGS = {
